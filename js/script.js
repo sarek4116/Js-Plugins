bf6c8a35c5c3cbbad7bd076c61cc7461ee2a78e1
Aleksander Sar <sarek4116@gmail.com>
2019-06-07 16:26:43 +0200

    add carousel and some styles

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false,
+    hash: true,
+});
+
+var flkty = new Flickity('.carousel');
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+});
+
+
+
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray(buttons);
+
+buttonGroup.addEventListener('click', function (event) {
+    // filter for button clicks
+    if (!matchesSelector(event.target, '.button')) {
+        return;
+    }
+    var index = buttons.indexOf(event.target);
+    flkty.select(index);
+});
