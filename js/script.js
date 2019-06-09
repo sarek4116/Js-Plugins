b33f06f390e10c11560b895669b5da3a17064c50
Aleksander Sar <sarek4116@gmail.com>
2019-06-09 14:35:45 +0200

    add google map

--- js/script.js
+++ js/script.js
@@ -48,3 +48,28 @@ var fullSlaid = Mustache.render(templateContainer, {
 
 
 results.insertAdjacentHTML('beforeend', fullSlaid);
+
+
+
+
+// GOOGLE MAP
+
+
+function initMap() {
+    // The location of Uluru
+    var uluru = {
+        lat: -25.344,
+        lng: 131.036
+    };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
